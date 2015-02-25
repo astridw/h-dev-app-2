f31dd8189fcefd635091aa7ea300749e0a41dac3
AstridW <AstridWcountee@gmail.com>
2015-02-25 10:38:40 -0600

    fixed required name for signup and displayed email once logged in

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-     validates :name, presence: true
+    #  validates :name, presence: true
      belongs_to :player
 
     rails_admin do
